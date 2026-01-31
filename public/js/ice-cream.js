3f2ac469ba3850dcc2eebf8c96efb96ba04efe60
AaronB22 <aaronthesnowboarder@live.com>
2026-01-30 19:12:45 -0800

    added fucntion that clears old errors

--- public/js/ice-cream.js
+++ public/js/ice-cream.js
@@ -8,7 +8,6 @@ document.getElementById("ice-cream-form").onsubmit = () => {
     let sugar = document.getElementById('sugar');
     let cup = document.getElementById('cup')
 
-    console.log(flavor.value)
     if (!name) {
         isValid = false;
         document.getElementById('err-name').style.display = "block";
@@ -17,14 +16,20 @@ document.getElementById("ice-cream-form").onsubmit = () => {
         isValid = false;
         document.getElementById('err-email').style.display = "block";
     }
-    if (flavor.value==='none') {
-        console.log("flavor is false")
+    if (flavor.value === 'none') {
         isValid = false;
         document.getElementById('err-flav').style.display = "block";
     }
-    if (!waffle.checked && !sugar.checked && !cup.checked){
+    if (!waffle.checked && !sugar.checked && !cup.checked) {
         document.getElementById('err-cone').style.display = "block";
 
     }
-        return isValid;
+    return isValid;
+}
+
+const clearErrors = () => {
+    let error = document.getElementsByClassName("err");
+    for (let i = 0; i < clearErrors.length; i++) {
+        error[i].style.display === 'none';
+    }
 }
