898f299919e929e5d0cdc4a46472f5fe9bd6f9b3
JHud <219741840+JamHudson@users.noreply.github.com>
2026-02-24 14:35:15 -0800

    Added admin page

--- app.js
+++ app.js
@@ -18,6 +18,10 @@ app.get("/", (req, res) => {
   res.render('home');
 });
 
+app.get('/admin',(req, res) => {
+  res.render('admin');
+});
+
 // Start the server and listen on the specified port
 app.listen(PORT, () => {
   console.log(`Server is running at http://localhost:${PORT}`);
